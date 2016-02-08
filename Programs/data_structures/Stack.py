a7c9602dc060b89c698aa3f38bd167a222a1d186
Siva Ninala <ninalasiva@gmail.com>
2016-02-09 00:10:28 +0530

    changing the file name and added logic to statck.py file

--- Programs/data_structures/Stack.py
+++ Programs/data_structures/Stack.py
@@ -1,11 +1,20 @@
 class Stack:
      def __init__(self):
+         '''
+         creates a new stack object
+         '''
          self.items = []
 
      def isEmpty(self):
+         '''
+         returns True if Stack is empty, else returns false
+         '''
          return self.items == []
 
      def push(self, item):
+         '''
+         adds a new item to the top of the stack, returns nothing.
+         '''
          self.items.append(item)
 
      def pop(self):
@@ -23,7 +32,30 @@ a = Stack()
 a.push(1)
 a.push(2)
 print a.pop()
-
 print a.isEmpty()
 
+# Another Implementation of Stack
+class Stack1:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.insert(0, item)
+    
+    def pop(self):
+        return self.items.pop(0)    
+    
+    def peek(self):
+        return self.items[0]
+    
+    def size(self):
+        return len(self.items)
+    
+s = Stack1()
+s.push('hello')
+s.push('true')
+print(s.pop())
 
