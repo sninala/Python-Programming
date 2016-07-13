83f3bbb8767469490576bebdeb485cf062ef31cd
Siva Ninala <ninalasiva@gmail.com>
2016-07-13 15:26:57 +0530

    adding selection sort program to repository

---
+++ Programs/data_structures/searching_sorting/selectionSort.py
@@ -0,0 +1,20 @@
+#selection sort improves on the bubble sort by making only one exchange for every pass through the list
+
+def selectionSort(items):
+    iterations = len(items) -1
+    while iterations:
+        maxValuePosition = 0
+        for current_position in range(iterations+1):
+            if items[current_position] > items[maxValuePosition]:
+                maxValuePosition = current_position
+        temp = items[iterations]
+        items[iterations] = items[maxValuePosition]
+        items[maxValuePosition] = temp
+        iterations -= 1
+    return items
+    
+
+
+items = [15, 70, 23, 44, 16, 99, 66, 32]
+print selectionSort(items)
+###O(nsquare)
