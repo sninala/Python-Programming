3280fe9d6008b3336e40ba7d7ebbc5981d00fcd8
Siva Ninala <ninalasiva@gmail.com>
2016-07-13 18:21:41 +0530

    adding few programs related to OOPs

---
+++ Programs/OOPS/Diamond_inheritance.py
@@ -0,0 +1,23 @@
+class A: #classic class
+    attr = 1 # 
+class B(A): # B and C both lead to A
+    pass
+class C(A):
+    attr = 2
+class D(B, C):
+    pass # Tries A before C
+
+x = D()
+print x.attr
+
+
+class A1(object): # New-style
+    attr = 1
+class B1(A1):
+    pass
+class C1(A1):
+    attr = 2
+class D1(B1, C1):
+    pass # Tries C before A
+y = D1()
+print y.attr
---
+++ Programs/OOPS/MRO_example.py
@@ -0,0 +1,19 @@
+class First(object):
+    def __init__(self):
+        print "first"
+
+class Second(First):
+    def __init__(self):
+        print "second"
+
+class Third(First):
+    def __init__(self):
+        print "third"
+
+class Fourth(Second, Third):
+    def __init__(self):
+        super(Fourth, self).__init__()
+        print "that's it"
+        
+        
+f = Fourth()
